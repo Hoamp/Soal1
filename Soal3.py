6336d5a16326befb8176babf797ab478996f3768
Hoamp <96369373+Hoamp@users.noreply.github.com>
2022-02-04 03:10:12 -0800

    Add files via upload

---
+++ Soal3.py
@@ -0,0 +1,7 @@
+def getAbsSum(a,b,c,d,e):
+    hasil=abs(a)+abs(b)+abs(c)+abs(d)+abs(e)
+    print(hasil)
+getAbsSum(2,-1,4,8,10)
+getAbsSum(-3,-4,-10,-2,-3)
+getAbsSum(2,4,6,8,10)
+getAbsSum(-1,0,0,0,0)
